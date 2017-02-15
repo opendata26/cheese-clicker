dbcff9efd55e3b7bffa25ee387dc2e8b4be700f1
Your Name <you@example.com>
2017-02-15 16:36:46 +0000

    remove stuff

--- build/CFile.d
+++
@@ -1,7 +0,0 @@
-CFile.o: /home/billy/Documents/gx2thingy/src/fs/CFile.cpp \
- /home/billy/Documents/gx2thingy/src/fs/CFile.hpp \
- /opt/devkitpro/libogc/include/gctypes.h
-
-/home/billy/Documents/gx2thingy/src/fs/CFile.hpp:
-
-/opt/devkitpro/libogc/include/gctypes.h:
