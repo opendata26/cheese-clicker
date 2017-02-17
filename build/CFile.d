a482457cea55739925b68238995358b6682e0161
Your Name <you@example.com>
2017-02-17 19:50:27 +0000

    Remove rct3 files

--- build/CFile.d
+++
@@ -1,7 +0,0 @@
-CFile.o: /home/billy/Documents/cheese-clicker/src/fs/CFile.cpp \
- /home/billy/Documents/cheese-clicker/src/fs/CFile.hpp \
- /opt/devkitpro/libogc/include/gctypes.h
-
-/home/billy/Documents/cheese-clicker/src/fs/CFile.hpp:
-
-/opt/devkitpro/libogc/include/gctypes.h:
--- build/StringTools.d
+++
@@ -1,5 +0,0 @@
-StringTools.o: \
- /home/billy/Documents/cheese-clicker/src/utils/StringTools.cpp \
- /opt/devkitpro/libogc/include/gctypes.h
-
-/opt/devkitpro/libogc/include/gctypes.h:
